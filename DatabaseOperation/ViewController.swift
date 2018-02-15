f7bf85a14d87c4beaffb738a4eaa502655952d11
Kushagra Saxena <kushagrasaxena@Kushagras-iMac.local>
2018-02-15 15:19:27 +0530

    Data fetch complete from amp server to label on IOS

--- DatabaseOperation/ViewController.swift
+++ DatabaseOperation/ViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //Our web service url
+    let URL_GET_TEAMS:String = "http://192.168.56.1/new/getusers.php"
+    //A string array to save all the names
+    var nameArray = [String]()
+    @IBOutlet weak var labelDisplay: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //calling the function that will fetch the json
+        getJsonFromUrl();
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +28,55 @@ class ViewController: UIViewController {
     }
 
 
-}
 
+    //this function is fetching the json from URL
+    func getJsonFromUrl(){
+        //creating a NSURL
+        let url = NSURL(string: URL_GET_TEAMS)
+        
+        //fetching the data from the url
+        URLSession.shared.dataTask(with: (url as URL?)!, completionHandler: {(data, response, error) -> Void in
+            
+            if let jsonObj = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? NSDictionary {
+                
+                //printing the json in console
+                print(jsonObj!.value(forKey: "users")!)
+                
+                //getting the avengers tag array from json and converting it to NSArray
+                if let heroeArray = jsonObj!.value(forKey: "users") as? NSArray {
+                    //looping through all the elements
+                    for heroe in heroeArray{
+                        
+                        //converting the element to a dictionary
+                        if let heroeDict = heroe as? NSDictionary {
+                            
+                            //getting the name from the dictionary
+                            if let name = heroeDict.value(forKey: "userName") {
+                                
+                                //adding the name to the array
+                                self.nameArray.append((name as? String)!)
+                            }
+                            
+                        }
+                    }
+}
+                OperationQueue.main.addOperation({
+                    //calling another function after fetching the json
+                    //it will show the names to label
+                    self.showNames()
+                })
+            }
+        }).resume()
+    }
+    
+    func showNames(){
+        //looing through all the elements of the array
+        for name in nameArray{
+            
+            //appending the names to label
+            labelDisplay.text = labelDisplay.text! + name + "\n";
+        }
+    }
+    
+    
+}
